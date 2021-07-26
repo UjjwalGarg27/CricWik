b30fbfa055c39db7773b2cb78d6b5b354c85843b
Ujjwal Garg <gargujjwal2014@gmail.com>
2021-07-26 16:13:48 +0530

    added open, close states

--- src/components/Parts/MatchCards/MatchCards.js
+++ src/components/Parts/MatchCards/MatchCards.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import "./style.css";
-import {Dialog} from "@material-ui/core";
 import { getMatchScore } from '../Home/API';
 
 const MatchCards = ({match}) => {
@@ -8,28 +7,31 @@ const MatchCards = ({match}) => {
     const [details, setDetails] = useState({});
     const [open, setOpen] = useState(false);
 
-    const handleClick = (id) => {
+    const handleBtn = (id) => {
+
+        if(match.matchStarted) {
+            setOpen(true);
+        } else {
+            alert("Match Not Started Yet");
+        }
+
         getMatchScore(id)
         .then((data) => {
             console.log(data);
             setDetails(data);
-            handleOpen();
         })
         .catch((error) => console.log(error));
-    }
+    };
 
-    const handleOpen = () => {
-        setOpen(true);
-      };
-    
-      const handleClose = () => {
+    const handleCloseBtn = () => {
         setOpen(false);
-      };
+    };
 
     return (
         <>
     <section className="container">
         <div className="card">
+            <div className="card-div2"><h1>Live Scores</h1></div>
                 <div className="card-div1">    
                     <h2 className="card-text"> {match.type === "" ? "N/A" : match.type} </h2>
                     <h2> {match.matchStarted === true ? "Match Started" : "Match Not Started Yet"} </h2>
@@ -44,105 +46,30 @@ const MatchCards = ({match}) => {
                 </div>
                 <div className="card-div4">
                 <div>
-                    <button onClick={() => {handleClick(match.unique_id)}}> <h1 className="card-btn"> See Score </h1> </button>
+                    <button onClick={() => {handleBtn(match.unique_id)}}> <h1 className="card-btn"> See Score </h1> </button>
                 </div>
                 </div>
 
-        </div>  
+            {open && ( 
+                <div className="card-div3">
+                <h4> Toss won by: {" "} </h4>
+                <div><h4>{match.toss_winner_team}</h4></div>
+                </div>
+            )}
+
+            {open && (
+                <div className="card-div2 describe"> 
+                   <div> <h4> {details.description} </h4> </div>
+                   <div className="card-div4 describe">
+                   <button onClick={() => {handleCloseBtn()}}> <h1 className="card-btn"> Close </h1> </button>
+                    </div>
+                </div>
+            )}
+    </div>  
     </section>
-
-    <Dialog open={open} onClose={handleClose} style={{width: "1500px", height: "500px"}}>
-            <div> {"Match Detail...."} </div>
-                <div>{details.stat}</div>
-                <div> 
-                    <h1> Match </h1>
-                    <span> {details.matchStarted ? "Started" : "Still not started"} </span>
-                </div>
-                <div> 
-                    <h1> Score </h1>
-                    <span> {details.score} </span>
-                </div>
-                <button onClick={handleClose}> Close </button>
-            </Dialog>
 
         </>
     )
 }
 
 export default MatchCards;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const MatchCards = ({match}) => {
-
-//     const [details, setDetails] = useState({});
-//     const [open, setOpen] = useState(false);
-
-//     const handleClick = (id) => {
-//         getMatchScore(id)
-//         .then((data) => {
-//             console.log(data);
-//             setDetails(data);
-//             handleOpen();
-//         })
-//         .catch((error) => console.log(error));
-//     }
-
-//     const handleOpen = () => {
-//         setOpen(true);
-//       };
-    
-//       const handleClose = () => {
-//         setOpen(false);
-//       };
-
-    // return (
-    //     <>
-    // <section className="container">
-    //     <div className="card">
-    //             <div className="card-div3">
-    //                 <h1 className="card-div3-text"> {match["team-1"]} </h1> 
-    //                 <h2> vs </h2> 
-    //                 <h1 className="card-div3-text"> {match["team-2"]} </h1>
-    //             </div>
-    //             <div className="card-div4">
-    //                 <button> <h1 className="card-btn1" onClick={() => {handleClick(match.unique_id)}}> Show Detail </h1> </button>
-    //                 <button> <h1 className="card-btn2"> Start Time {new Date(match.dateTimeGMT).toLocaleString()} </h1> </button>
-    //             </div>
-    //     </div>  
-//     </section>
-//             <Dialog open={open} onClose={handleClose} style={{width: "1500px", height: "500px"}}>
-//                 <div> {"Match Detail...."} </div>
-//                 <div>{details.stat}</div>
-//                 <div> 
-//                     <h1> Match </h1>
-//                     <span> {details.matchStarted ? "Started" : "Still not started"} </span>
-//                 </div>
-//                 <div> 
-//                     <h1> Score </h1>
-//                     <span> {details.score} </span>
-//                 </div>
-//                 <button onClick={handleClose}> Close </button>
-//             </Dialog>
-
-//         </>
-//     )
-// }
-
-// export default MatchCards;
