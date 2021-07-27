02021599e23880dce5b46fd14cc11a7154fbede0
Ujjwal Garg <gargujjwal2014@gmail.com>
2021-07-28 00:46:55 +0530

    updated tableau page

--- src/components/Parts/Tableau/Tableau.js
+++ src/components/Parts/Tableau/Tableau.js
@@ -9,13 +9,64 @@ const Tableau = () => {
         <Navbar />
 
         <div className="tableau">
-        <h1> Data 1 </h1>
-            <TableauReport url="https://public.tableau.com/views/GenderbifurcationwrtDepartment/GenderbifurcationwrtDepartment" />
+        <h1> DATA ANALYSIS 1 </h1>
+        </div>
+        <div>
+            <TableauReport url="https://public.tableau.com/views/CricketVisualization-4/DIFFERENTVENUESDETAILS" />
         </div>
 
         <div className="tableau">
-        <h1> Data 2 </h1>
-            <TableauReport url="https://public.tableau.com/views/MonthlySalarybasedonMaritalStatuswrtEducationLevel/MonthlySalarybasedonMaritalStatuswrtEducationLevel" />
+        <h1> DATA ANALYSIS 2 </h1>
+        </div>
+        <div>
+            <TableauReport url="https://public.tableau.com/views/CricketVisualization-3/IMPORTANCEOFTOSSINWINNINGTHEGAME" />
+        </div>
+
+        <div className="tableau">
+        <h1> DATA ANALYSIS 3 </h1>
+        </div>
+        <div>
+            <TableauReport url="https://public.tableau.com/views/CricketVisualization-5/TOTALNUMBEROFMATCHESWONBYINDIVIDUALTEAMS" />
+        </div>
+
+        <div className="tableau">
+        <h1> DATA ANALYSIS 4 </h1>
+            <TableauReport url="https://public.tableau.com/views/CricketVisualization/TOTALRUNSSCOREDBYTHEBATSMEN" />
+        </div>
+
+        <div className="tableau">
+        <h1> DATA ANALYSIS 5 </h1>
+        </div>
+        <div>
+            <TableauReport url="https://public.tableau.com/views/CricketVisualization-8/Numberof4sand6sscored" />
+        </div>
+
+        <div className="tableau">
+        <h1> DATA ANALYSIS 6 </h1>
+        </div>
+        <div>
+            <TableauReport url="https://public.tableau.com/views/CricketVisualization-1/TEAMPLAYERSTYPESOFDISMISSALS" />
+        </div>
+
+        <div className="tableau">
+        <h1> DATA ANALYSIS 7 </h1>
+        </div>
+        <div>
+            <TableauReport url="https://public.tableau.com/views/CricketVisualization-7/BowlingTypeswiththeiraverageEconomyandrunsgiven" />
+        </div>
+
+        <div className="tableau">
+        <h1> DATA ANALYSIS 8 </h1>
+        </div>
+        <div>
+            <TableauReport url="https://public.tableau.com/views/CricketVisualization-2/UNWANTEDRUNSGIVENBYDIFFERENTTEAM" />
+        </div>
+
+        <div className="tableau">
+        <h1> DATA ANALYSIS 9 </h1>
+        </div>
+        <div>
+            <TableauReport url="https://public.tableau.com/views/CricketVisualization-6/PLAYEROFTHEMATCH" />
         </div>
         </>
     )
