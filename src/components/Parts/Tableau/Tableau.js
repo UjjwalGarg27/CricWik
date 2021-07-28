871a13d8382e1907b48f35bfb9a7ea9ccc19f408
Ujjwal Garg <gargujjwal2014@gmail.com>
2021-07-28 13:39:41 +0530

    final commit

--- src/components/Parts/Tableau/Tableau.js
+++ src/components/Parts/Tableau/Tableau.js
@@ -18,56 +18,56 @@ const Tableau = () => {
         <div className="tableau">
         <h1> DATA ANALYSIS 2 </h1>
         </div>
-        <div>
+        <div className="tableau-img">
             <TableauReport url="https://public.tableau.com/views/CricketVisualization-3/IMPORTANCEOFTOSSINWINNINGTHEGAME" />
         </div>
 
         <div className="tableau">
         <h1> DATA ANALYSIS 3 </h1>
         </div>
-        <div>
+        <div className="tableau-img">
             <TableauReport url="https://public.tableau.com/views/CricketVisualization-5/TOTALNUMBEROFMATCHESWONBYINDIVIDUALTEAMS" />
         </div>
 
         <div className="tableau">
         <h1> DATA ANALYSIS 4 </h1>
         </div>
-        <div>
+        <div className="tableau-img">
             <TableauReport url="https://public.tableau.com/views/CricketVisualization/TOTALRUNSSCOREDBYTHEBATSMEN" />
         </div>
 
         <div className="tableau">
         <h1> DATA ANALYSIS 5 </h1>
         </div>
-        <div>
+        <div className="tableau-img">
             <TableauReport url="https://public.tableau.com/views/CricketVisualization-8/Numberof4sand6sscored" />
         </div>
 
         <div className="tableau">
         <h1> DATA ANALYSIS 6 </h1>
         </div>
-        <div>
+        <div className="tableau-img">
             <TableauReport url="https://public.tableau.com/views/CricketVisualization-1/TEAMPLAYERSTYPESOFDISMISSALS" />
         </div>
 
         <div className="tableau">
         <h1> DATA ANALYSIS 7 </h1>
         </div>
-        <div>
+        <div className="tableau-img">
             <TableauReport url="https://public.tableau.com/views/CricketVisualization-7/BowlingTypeswiththeiraverageEconomyandrunsgiven" />
         </div>
 
         <div className="tableau">
         <h1> DATA ANALYSIS 8 </h1>
         </div>
-        <div>
+        <div className="tableau-img">
             <TableauReport url="https://public.tableau.com/views/CricketVisualization-2/UNWANTEDRUNSGIVENBYDIFFERENTTEAM" />
         </div>
 
         <div className="tableau">
         <h1> DATA ANALYSIS 9 </h1>
         </div>
-        <div>
+        <div className="tableau-img">
             <TableauReport url="https://public.tableau.com/views/CricketVisualization-6/PLAYEROFTHEMATCH" />
         </div>
 
